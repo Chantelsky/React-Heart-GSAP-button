842121b333ca7fbdd3a0f972e02aa438cabc1021
Chantel Sky <35864046+Chantelsky@users.noreply.github.com>
2020-01-20 22:21:28 +1000

    incrementing when clicked

--- src/components/heartbutton.js
+++ src/components/heartbutton.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { ReactComponent } from "../images/cupid.svg";
 
-const heartButton = () => {
+const heartButton = ({ count, incrementCount }) => {
   return (
     <div>
-      <button>
+      <button onClick={incrementCount}>
         <div className="cupid-icon">
           <ReactComponent alt={"cupid"} />
+          <div>{count}</div>
         </div>
       </button>
     </div>
   );
+  s;
 };
 
 export default heartButton;
