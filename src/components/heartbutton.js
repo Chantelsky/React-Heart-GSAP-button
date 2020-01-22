2dd5e9512fc8af663a9f183f62f584d3e9265c76
Chantel Sky <35864046+Chantelsky@users.noreply.github.com>
2020-01-22 19:43:20 +1000

    heart flies up on click

--- src/components/heartbutton.js
+++ src/components/heartbutton.js
@@ -1,26 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ReactComponent } from "../images/cupid.svg";
 import { ReactComponent as Heart } from "../images/heart.svg";
 import gsap from "gsap";
 
 const HeartButton = ({ count, incrementCount }) => {
+  // let HeartDiv = React.useRef(null);
+
+  const [tl] = useState(gsap.timeline({ paused: true }));
+
+  const [clickAnimation, setClickAnimation] = useState();
+
+  useEffect(() => {
+    setClickAnimation(
+      tl.to("#Layer_1", {
+        duration: 0.75,
+        ease: "slow(0.7, 0.7, false)",
+        opacity: 1,
+        display: "block",
+        y: -200
+      })
+    );
+  }, [tl]);
+
+  useEffect(() => {
+    tl.time(0);
+  }, [count, tl]);
+
   const handleClick = () => {
-    gsap.to("#Layer_1", {
-      duration: 2.5,
-      ease: "slow(0.7, 0.7, false)",
-      opacity: 1,
-      y: -100
-    });
-    //update state in app
     incrementCount();
+    clickAnimation.play();
   };
 
   return (
     <div className="container">
-      <button className="btn" onClick={handleClick}>
+      <button className="btn" onClick={() => handleClick()}>
         <div className="cupid-icon">
           <ReactComponent alt={"cupid"} />
-          <Heart />
+          <Heart className="heart-icon" />
           <div className="clicky">{count}</div>
         </div>
       </button>
